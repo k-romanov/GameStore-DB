014b2dc1ed0bc736517fdc711fb575c7d948f1ef
Redych_the_kid <79579497+Redych-the-kid@users.noreply.github.com>
2023-05-20 19:12:55 +0700

    Add files via upload

--- BaseDACs/src/main/java/Main.java
+++ BaseDACs/src/main/java/Main.java
@@ -1,7 +1,10 @@
+import controllers.DAC;
+import controllers.ReviewDAC;
 import controllers.UsersDAC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 import java.util.Scanner;
@@ -10,24 +13,31 @@ public class Main {
     public static void main(String[] args){
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamestore", "root", "homuhomu");
-            System.out.print("Enter username");
+            System.out.print("Enter username:");
             Scanner scanner = new Scanner(System.in);
             String username = scanner.nextLine();
             System.out.print("Enter password:");
             String password = scanner.nextLine();
-            UsersDAC dac = new UsersDAC(connection, false);
-            if(Objects.equals(password, dac.get_password(username))){
+            DAC.setConnection(connection);
+            if(Objects.equals(password, UsersDAC.get_password(username))){
                 System.out.println("Welcome, " + username);
             } else{
                 System.out.println("Wrong username or password!");
             }
-            if(dac.checkAdmin(username)){
+            if(UsersDAC.checkAdmin(username)){
                 System.out.println("You are an admin!");
-                dac.setAdmin(true);
+                DAC.setAdmin(true);
             }
-            System.out.print("Search for a user:");
+            System.out.print("Search for a user id:");
             String search = scanner.nextLine();
-            //TODO: User search
+            for(int i: UsersDAC.getUserIDs(search)){
+                System.out.println(UsersDAC.getUserName(i));
+            }
+            System.out.println("Get game reviews");
+            int id = Integer.parseInt(scanner.nextLine());
+            for (String i: ReviewDAC.getReviews(id)){
+                System.out.println(i);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
