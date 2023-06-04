b36736682998d5033eff3cae9b593981f64f8600
k-romanov <k.romanov@g.nsu.ru>
2023-06-04 22:46:11 +0700

    GameListView

---
+++ BaseDACs/src/main/java/views/GameListView.java
@@ -0,0 +1,62 @@
+package views;
+
+import controllers.GameDAC;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GameListView extends View{
+    private String _queryString = null;
+    public void addQuery(String query){
+        _queryString = query;
+    }
+    public void view() throws SQLException {
+        int choice = 0;
+        int lb = 0;
+        int mul = 10;
+        while(choice != 5){
+            ResultSet games;
+            if(_queryString != null) {
+                games = GameDAC.getGameListByQuery(_queryString, lb * mul, (lb + 1) * mul);
+            }
+            else {
+                games = GameDAC.getGameList(lb * mul, (lb + 1) * mul);
+            }
+            games.beforeFirst();
+            int count = 1;
+            while(games.next()){
+                System.out.println(String.format("%d: %s", lb + count, games.getString("game_title")));
+                count += 1;
+            }
+            choice = Integer.parseInt(new Scanner(System.in).nextLine());
+            switch(choice) {
+                case(1): lb += 1;
+                case(2):{if (lb >= 1) {lb -= 1;}}
+                case(3):{
+                    int gameChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+                    while((gameChoice < 0) || (gameChoice >= mul)){
+                        System.out.println("Invalid game number!");
+                        gameChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+                    }
+                    games.absolute(gameChoice);
+                    GameView.view(games.getString("game_id"));
+                }
+                case(4):{
+                    if(!isAdmin){
+                        System.out.println("Not an admin!");
+                    }
+                    else{
+                        int gameChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+                        while((gameChoice < 0) || (gameChoice >= mul)){
+                            System.out.println("Invalid game number!");
+                            gameChoice = Integer.parseInt(new Scanner(System.in).nextLine());
+                        }
+                        games.absolute(gameChoice);
+                        GameDAC.removeGame(games.getString("game_id"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BaseDACs/src/main/java/views/UserListView.java
@@ -0,0 +1,4 @@
+package views;
+
+public class UserListView extends View{
+}
