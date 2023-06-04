18a0173f4b36e7e8bff37df057924f2880a83d5f
k-romanov <k.romanov@g.nsu.ru>
2023-06-05 00:08:54 +0700

    hotfix ruight ballx

--- BaseDACs/src/main/java/controllers/ReviewDAC.java
+++ BaseDACs/src/main/java/controllers/ReviewDAC.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class ReviewDAC {
     public static ResultSet getReviews(int gameID, int start, int end) throws SQLException {
-        return DAC.getSet(String.format("SELECT review_id, text_review, upvote_countdown, downvote_count, funny_count " +
+        return DAC.getSet(String.format("SELECT review_id, text_review, upvote_countdown, downvote_countdown, funny_countdown " +
                         "FROM Reviews JOIN Games ON Games.game_id = Reviews.game_id WHERE user_id = %d LIMIT %d, %d", gameID, start, end));
     }
 
